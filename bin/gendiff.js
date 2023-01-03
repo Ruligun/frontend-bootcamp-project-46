3d0efca81f80d6d6a6e0501e3c505b77a2e07cef
ruligun <ruligun@gmail.com>
2023-01-03 08:33:04 +0100

    Step 2: add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,6 @@ import {program} from 'commander';
 program
     .description('Compares two configuration files and shows a difference')
     .version('1.0.0', '-V, --version', 'output the version number')
+    .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format');
 program.parse();
